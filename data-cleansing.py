eaeed129784b734553965c752fab8b6029225a18
AV-01 <aryavratmishra007@gmail.com>
2024-07-25 09:57:34 -0700

    Better Cleaning Script
    Ignoring more useless info

--- data-cleansing.py
+++ data-cleansing.py
@@ -12,19 +12,22 @@ def read_file(filename):
     lines = set()
     with open(os.path.join(directory, filename), 'r', encoding='utf-8') as file:
         for line in file:
-            lines.add(line.strip())
+            lines.add(line.strip().replace(u'\xa0', u' '))
     return lines
 
-def add_to_file(lines):
+def add_to_file(lines, filename = ""):
     with open(output_file, 'a', encoding='utf-8') as file:
+        file.write(filename+"\n")
         for line in lines:
             file.write(line + '\n')
 
 for filename in os.listdir(directory):
+    if "xlsx" in filename or "photo" in filename.lower() or "spanish" in filename.lower() or "jpeg" in filename:
+        continue
     if filename.endswith('.txt'):
         print("Cleansing: " + filename)
         lines = read_file(filename)
-        add_to_file(lines)
+        add_to_file(lines, filename)
 
 lines_seen = set() # holds lines already seen
 outfile = open("cleansed-data/all-data-cleansed.txt", "w", encoding="utf-8")
