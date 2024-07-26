caa7960d462d1e8d46168f858044153afbafb42c
AV-01 <aryavratmishra007@gmail.com>
2024-07-26 12:02:30 -0700

    Got all extra links
    Extra links are links that aren't part of the requested domain

--- test.py
+++ test.py
@@ -1,57 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import os
+import time
 
+start = time.time()
 
-# Making a GET request
-r = requests.get('https://rhs.rocklinusd.org/Counseling/Class-of-2026/index.html')
+lines_seen = set() # holds lines already seen
+outfile = open("extra-links-final.txt", "w", encoding="utf-8")
+for line in open("extra-links.txt", "r", encoding="utf-8"):
+    if line not in lines_seen: # not a duplicate
+        outfile.write(line)
+        lines_seen.add(line)
+outfile.close()
 
-# check status code for response received
-# success code - 200
-print(r)
-
-# Parsing the HTML
-soup = BeautifulSoup(r.content, 'html.parser')
-
-# s = soup.find('div', class_='col')
-content_h1 = soup.find_all('h1')
-content_h2 = soup.find_all('h2')
-content_h3 = soup.find_all('h3')
-content_h4 = soup.find_all('h4')
-content_h5 = soup.find_all('h5')
-content_h6 = soup.find_all('h6')
-content_p = soup.find_all('p')
-content_div = soup.find_all('div')
-
-all_content = [content_h1, content_h2, content_h3, content_h4, content_h5, content_h6, content_p]
-# s1 = soup.find('div', class_='col')
-
-# for content in content_div:
-#     print(content.get_text().strip())
-# print(all_content[0][0].get_text())
-element = soup.find('body')
-
-#get_text with strip set to true
-# text_content = element.get_text(" ",strip=True)
-
-for script in soup(["script", "style", "nav", "header", "footer"]):
-    script.extract()
-
-# Get the main text content
-main_text = soup.get_text(strip=True)
-main_content = soup.find("main")
-if main_content:
-    main_text = main_content.get_text("\n",strip=True)
-
-
-# print(main_text)
-
-
-# print("Text: \n", text_content)
-
-
-response = requests.get("https://rhs.rocklinusd.org/documents/2021_School_Accountability_Report_Card_Rocklin_High_School.pdf")
-
-# Write content in pdf file
-pdf = open("test.pdf", 'wb')
-pdf.write(response.content)
-pdf.close()
+end = time.time()
+total_time = end-start
+print("Total time: " + str(total_time))
